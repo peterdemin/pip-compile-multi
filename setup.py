a70dd534fc9b9bea3e9048018eb4d3f2e9f1359c
Peter Demin <peterdemin@gmail.com>
2018-01-16 10:24:59 -0500

    Support more Python versions

--- setup.py
+++ setup.py
@@ -47,6 +47,9 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     entry_points={
         'console_scripts': [
