eb24073c705977180fb2a86d39944e264c77eee4
Peter Demin <peterdemin@gmail.com>
2018-01-20 02:57:35 +0000

    Add verify command

--- pipcompilemulti.py
+++ pipcompilemulti.py
@@ -10,10 +10,13 @@ Internal dependencies are soft-pinned using ~=
 ".post23423" version postfixes are truncated
 """
 
+from __future__ import unicode_literals
+
 import os
 import re
 import sys
 import glob
+import hashlib
 import logging
 import itertools
 import subprocess
@@ -68,14 +71,7 @@ OPTIONS = {
               help='File path with custom header text for generated files')
 def cli(ctx, compatible, post, directory, in_ext, out_ext, header):
     """Recompile"""
-    if ctx.invoked_subcommand is None:
-        recompile(compatible, post, directory, in_ext, out_ext, header)
-    else:
-        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
-
-
-def recompile(compatible, post, directory, in_ext, out_ext, header):
-    """Update global OPTIONS and run main"""
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
     OPTIONS.update({
         'compatible_patterns': compatible,
         'allow_post': set(post),
@@ -84,19 +80,14 @@ def recompile(compatible, post, directory, in_ext, out_ext, header):
         'out_ext': out_ext,
         'header_file': header or None,
     })
-    main()
+    if ctx.invoked_subcommand is None:
+        return recompile()
 
 
-@cli.command()
-def sync():
-    click.echo('The subcommand')
-
-
-def main():
+def recompile():
     """
     Compile requirements files for all environments.
     """
-    logging.basicConfig(level=logging.DEBUG)
     pinned_packages = {}
     env_confs = discover(
         os.path.join(
@@ -106,9 +97,9 @@ def main():
     )
     if OPTIONS['header_file']:
         with open(OPTIONS['header_file']) as fp:
-            header_text = fp.read()
+            base_header_text = fp.read()
     else:
-        header_text = DEFAULT_HEADER
+        base_header_text = DEFAULT_HEADER
     for conf in env_confs:
         rrefs = recursive_refs(env_confs, conf['name'])
         env = Environment(
@@ -119,6 +110,7 @@ def main():
         logger.info("Locking %s to %s. References: %r",
                     env.infile, env.outfile, sorted(rrefs))
         env.create_lockfile()
+        header_text = generate_hash_comment(env.infile) + base_header_text
         env.replace_header(header_text)
         env.add_references(conf['refs'])
         pinned_packages[conf['name']] = set(env.packages)
@@ -451,5 +443,59 @@ def order_by_refs(envs):
     ]
 
 
-if __name__ == '__main__':
-    main()
+@cli.command()
+@click.pass_context
+def verify(ctx):
+    """
+    For each environment verify hash comments and report failures.
+    If any failure occured, exit with code 1.
+    """
+    env_confs = discover(
+        os.path.join(
+            OPTIONS['base_dir'],
+            '*.' + OPTIONS['in_ext'],
+        )
+    )
+    success = True
+    for conf in env_confs:
+        env = Environment(name=conf['name'])
+        logger.info("Verifying that %s was generated from %s.",
+                    env.outfile, env.infile)
+        current_comment = generate_hash_comment(env.infile)
+        existing_comment = parse_hash_comment(env.outfile)
+        if current_comment == existing_comment:
+            logger.info("Success - comments match.")
+        else:
+            logger.error("FAILURE!")
+            logger.error("Expecting: %s", current_comment.strip())
+            logger.error("Found:     %s", existing_comment.strip())
+            success = False
+    if not success:
+        ctx.exit(1)
+
+
+def generate_hash_comment(file_path):
+    """
+    Read file with given file_path and return string of format
+
+        # SHA1:da39a3ee5e6b4b0d3255bfef95601890afd80709
+
+    which is hex representation of SHA1 file content hash
+    """
+    with open(file_path, 'rb') as fp:
+        hexdigest = hashlib.sha1(fp.read().strip()).hexdigest()
+    return "# SHA1:{0}\n".format(hexdigest)
+
+
+def parse_hash_comment(file_path):
+    """
+    Read file with given file_path line by line,
+    return the first line that starts with "# SHA1:", like this:
+
+        # SHA1:da39a3ee5e6b4b0d3255bfef95601890afd80709
+    """
+    with open(file_path) as fp:
+        for line in fp:
+            if line.startswith("# SHA1:"):
+                return line
+    return None
