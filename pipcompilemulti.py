768e0add5a85637e9c890df611b5196cc90ba66e
Peter Demin <peterdemin@gmail.com>
2018-01-23 18:37:24 +0000

    Bump version: 1.1.5 → 1.1.6

--- pipcompilemulti.py
+++ pipcompilemulti.py
@@ -28,7 +28,7 @@ from toposort import toposort_flatten
 
 __author__ = 'Peter Demin'
 __email__ = 'peterdemin@gmail.com'
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 
 
 logger = logging.getLogger("pip-compile-multi")
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "1.1.5"
+version = "1.1.6"
 
 
 with open('README.rst') as fp:
