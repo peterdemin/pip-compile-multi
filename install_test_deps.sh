c9d31603733bb773090de28235bcf8e78a4073e2
Peter Demin <peterdemin@gmail.com>
2018-02-09 19:51:18 +0000

    Install with hashes on Travis, without on AppVeyor

--- install_test_deps.sh
+++ install_test_deps.sh
@@ -5,8 +5,8 @@ python --version 2>&1 | grep -q 'Python 3'
 if [ $? -eq 0 ]
 then
     # Python 3
-    exec pip install -r requirements/test.txt
+    exec pip install -r requirements/test.hash
 else
     # Python 2 or PyPy
-    exec pip install -r requirements/test.txt -r requirements/py27.txt
+    exec pip install -r requirements/test.hash -r requirements/py27.hash
 fi
