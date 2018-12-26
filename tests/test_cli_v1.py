c997ca3e3c9e2aad55187d3c75854cdcda95ea9a
Petr <petr.demin@nih.gov>
2018-12-26 17:19:49 -0500

    run v1 with hashes to maintain files SHAs

--- tests/test_cli_v1.py
+++ tests/test_cli_v1.py
@@ -5,12 +5,22 @@ import pytest
 from pipcompilemulti.cli_v1 import cli
 
 
-@pytest.mark.parametrize('command', ['--no-upgrade', '--upgrade', 'verify'])
+@pytest.mark.parametrize('command', ['--no-upgrade', '--upgrade'])
 def test_v1_command_exits_with_zero(command):
     """Run pip-compile-multi on self"""
     runner = CliRunner()
-    parameters = [command]
-    if command != 'verify':
-        parameters.extend(['--only-name', 'local'])
+    parameters = ['--only-name', 'local']
+    parameters.append(command)
     result = runner.invoke(cli, parameters)
+    parameters[:0] = ['--generate-hashes', 'local',
+                      '--in-ext', 'txt',
+                      '--out-ext', 'hash']
+    result = runner.invoke(cli, parameters)
+    assert result.exit_code == 0
+
+
+def test_v1_verify_exits_with_zero():
+    """Run pip-compile-multi on self"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['verify'])
     assert result.exit_code == 0
