93c861b4d27c98eadb6354e0ed685801d379f3c1
Peter Demin <peter.demin@toptal.com>
2019-09-14 18:10:25 -0400

    run acceptance tests in temp dir

--- tests/test_cli_v1.py
+++ tests/test_cli_v1.py
@@ -1,8 +1,24 @@
 """End to end tests for CLI v2"""
 
+import os
+import tempfile
+import shutil
+
 from click.testing import CliRunner
 import pytest
 from pipcompilemulti.cli_v1 import cli
+from pipcompilemulti.options import OPTIONS
+
+
+@pytest.fixture(autouse=True)
+def requirements_dir():
+    """Create temporary requirements directory for test time."""
+    tmp_dir = tempfile.mkdtemp()
+    os.rmdir(tmp_dir)
+    shutil.copytree('requirements', tmp_dir)
+    OPTIONS['base_dir'] = tmp_dir
+    yield
+    shutil.rmtree(tmp_dir)
 
 
 @pytest.mark.parametrize('command', ['--no-upgrade', '--upgrade',
