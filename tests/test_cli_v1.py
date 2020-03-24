04ad753bc8c16635e9da55c82c3c84e01630aac2
Peter Demin <peter.demin@moveworks.ai>
2020-03-23 22:06:37 -0400

    fix tests for Python2.7

--- tests/test_cli_v1.py
+++ tests/test_cli_v1.py
@@ -1,5 +1,6 @@
 """End to end tests for CLI v2"""
 
+import sys
 from click.testing import CliRunner
 import pytest
 from pipcompilemulti.cli_v1 import cli
@@ -19,11 +20,16 @@ def requirements_dir():
                                      '--upgrade-package=pip-tools'])
 def test_v1_command_exits_with_zero(command):
     """Run pip-compile-multi on self"""
+    local = (
+        'local'
+        if sys.version_info[0] >= 3
+        else 'local27'
+    )
     runner = CliRunner()
-    parameters = ['--only-name', 'local']
+    parameters = ['--only-name', local]
     parameters.append(command)
     result = runner.invoke(cli, parameters)
-    parameters[:0] = ['--generate-hashes', 'local',
+    parameters[:0] = ['--generate-hashes', local,
                       '--in-ext', 'txt',
                       '--out-ext', 'hash']
     result = runner.invoke(cli, parameters)
