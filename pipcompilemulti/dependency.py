496f455ed69498b116d8a7c461a54250c8f24fbd
Jake Schmidt <jacob.schmidt@recursionpharma.com>
2022-04-01 18:44:17 -0600

    fix regex

--- pipcompilemulti/dependency.py
+++ pipcompilemulti/dependency.py
@@ -103,7 +103,7 @@ class Dependency(object):  # pylint: disable=too-many-instance-attributes
         if regular:
             self.package = regular.group('package')
             _version = regular.group('version').strip()
-            self.version = re.sub(r"\.(0)\d+", "", _version)  # strip leading zeros
+            self.version = re.sub(r"(?<=\.)0+(?=\d+)", "", _version)  # strip leading zeros
             self.hashes = (regular.group('hashes') or '').strip()
             self.comment = (regular.group('comment') or '').rstrip()
             self.comment_span = self._adjust_span(regular.span('comment'), regular)
