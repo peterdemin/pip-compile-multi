bf3f770d24cb2ce9882ab962a0a3702d8073f26e
Peter Demin <peterdemin@gmail.com>
2025-06-19 13:00:16 -0700

    Bump version: 3.2.0 → 3.2.1

--- pipcompilemulti/__init__.py
+++ pipcompilemulti/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Peter Demin'
 __email__ = 'peterdemin@gmail.com'
-__version__ = '3.2.0'
+__version__ = '3.2.1'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = "3.2.0"
+VERSION = "3.2.1"
 
 
 README = """
